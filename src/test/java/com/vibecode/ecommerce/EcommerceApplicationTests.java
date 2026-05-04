8d64ecec11c3c446b4971a10c0e81d3b08ee82ba
Veera Venkata Sai Mane <95539535+Venkat0629@users.noreply.github.com>
2026-05-04 11:11:40 +0530

    Configure explicit Oracle schema for customer tables

---
+++ src/test/java/com/vibecode/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vibecode.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
